f2808858d65b89404a52fea4a9d13a1896ac24cf
Darrel G <94090322+DarrelG@users.noreply.github.com>
2025-02-13 11:12:18 +0700

    Update page.tsx

--- src/app/Pages/signUp/page.tsx
+++ src/app/Pages/signUp/page.tsx
@@ -1,39 +1,45 @@
 "use client";
+
+import Navbar from "~/app/Components/navbar";
 import SignForm from "~/app/Components/signForm";
 import Image from "next/image";
 import chad from "~/app/Assets/chad.jpg";
+import { api } from "~/trpc/react";
+import { useRouter } from "next/navigation";
+import { useState } from "react";
 import { useSignUp } from "../../api/SignUp/SignUp";
 
 const SignUp = () => {
-  const { handleSubmit, isPending, errors } = useSignUp();
-  return (
-    <div className="flex flex-col items-center justify-center">
-      <div className="flex flex-col items-center gap-4 pt-5">
-        <Image
-          src={chad}
-          alt="chad"
-          className="h-11 w-11 rounded-full object-cover"
-        />
-        <h2 className="text-3xl font-bold">Sign Up for Free</h2>
-        <form
-          onSubmit={handleSubmit}
-          className="flex flex-col items-center gap-3 rounded-lg border-[1px] border-black p-14 shadow-lg"
-        >
-          <SignForm
-            label={["Username", "Email", "Password"]}
-            type={["text", "text", "password"]}
-            placeholder={[
-              "Enter your username",
-              "Enter your email",
-              "Enter your password",
-            ]}
-            disabled={isPending}
-            error={errors}
-          />
-        </form>
-      </div>
-    </div>
-  );
+    const { handleSubmit, isPending, errors } = useSignUp();
+    return (
+        <div className="flex flex-col items-center justify-center overflow-hidden">
+            <Navbar type={false} />
+            <div className="flex flex-col items-center gap-4 pt-5 bg-cream w-full h-[calc(100vh-4rem)] overflow-hidden">
+                <Image
+                    src={chad}
+                    alt="chad"
+                    className="h-11 w-11 rounded-full object-cover"
+                />
+                <h2 className="text-3xl font-bold">Sign Up for Free</h2>
+                <form
+                    onSubmit={handleSubmit}
+                    className="flex flex-col items-center gap-3 rounded-lg border-[1px] border-black p-14 shadow-lg bg-sageGreen"
+                >
+                    <SignForm
+                        label={["Username", "Email", "Password"]}
+                        type={["text", "text", "password"]}
+                        placeholder={[
+                            "Enter your username",
+                            "Enter your email",
+                            "Enter your password",
+                        ]}
+                        disabled={isPending}
+                        error={errors}
+                    />
+                </form>
+            </div>
+        </div>
+    );
 };
 
 export default SignUp;
