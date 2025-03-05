07134bdce583de0435db2c56e5411b753a1550ac
ThotDestroyerXXX <151725216+ThotDestroyerXXX@users.noreply.github.com>
2025-03-05 16:09:50 +0700

    add page daftar sebagai organisasi

---
+++ src/app/Components/modal.tsx
@@ -0,0 +1,32 @@
+import Link from "next/link";
+import { type ReactNode } from "react";
+
+interface ModalProps {
+  isOpen: boolean;
+  onClose: () => void;
+  children: ReactNode;
+}
+
+const Modal = ({ isOpen, onClose, children }: ModalProps) => {
+  if (!isOpen) return null;
+
+  return (
+    <button
+      className="fixed inset-0 flex cursor-default items-center justify-center bg-black bg-opacity-50"
+      onClick={onClose}
+    >
+      <div className="relative flex w-full max-w-[30rem] flex-col gap-6 rounded-lg bg-white p-6 shadow-lg">
+        <Link
+          href="/Pages/signIn"
+          className="absolute right-5 top-3 text-xl font-bold text-gray-500 hover:text-gray-700"
+          onClick={onClose}
+        >
+          &#x2715; {/* Close button */}
+        </Link>
+        {children}
+      </div>
+    </button>
+  );
+};
+
+export default Modal;
--- src/app/Components/navbar.tsx
+++ src/app/Components/navbar.tsx
@@ -6,9 +6,12 @@ import Link from "next/link";
 import { type Session } from "~/lib/auth";
 import SignOutButton from "./signOutButton";
 import { usePathname } from "next/navigation";
+import AlertModal from "./signUpType";
+import { useState } from "react";
 
 const Navbar = ({ session }: { session: Session | null }) => {
   const pathname = usePathname();
+  const [modalOpen, setModalOpen] = useState(false);
   return (
     <div className="flex h-16 w-full items-center justify-between border-b-2 border-black bg-sageGreen pb-2 pl-10 pr-10 pt-2 text-black">
       <div className="flex items-center gap-3">
@@ -46,17 +49,26 @@ const Navbar = ({ session }: { session: Session | null }) => {
             <SignOutButton />
           </>
         ) : (
-          <Link
-            href={
-              pathname == "/Pages/signIn" ? "/Pages/signUp" : "/Pages/signIn"
-            }
-          >
-            <button className="rounded-md bg-green-600 pb-2 pl-4 pr-4 pt-2 text-white">
+          <Link href={pathname == "/Pages/signUp" ? "/Pages/signIn" : ""}>
+            <button
+              className="rounded-md bg-green-600 pb-2 pl-4 pr-4 pt-2 text-white"
+              data-modal-target={`${pathname != "/Pages/signUp" ? "popup-modal" : ""}`}
+              data-modal-toggle={`${pathname != "/Pages/signUp" ? "popup-modal" : ""}`}
+              type="button"
+              onClick={() => {
+                if (pathname != "/Pages/signUp") {
+                  setModalOpen(true);
+                } else {
+                  setModalOpen(false);
+                }
+              }}
+            >
               {pathname == "/Pages/signIn" ? "Sign Up" : "Sign In"}
             </button>
           </Link>
         )}
       </div>
+      <AlertModal isOpen={modalOpen} onClose={() => setModalOpen(false)} />
     </div>
   );
 };
