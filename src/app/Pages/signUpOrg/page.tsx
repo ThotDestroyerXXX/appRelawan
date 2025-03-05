3a836189585bad4d28d2fa076b95f74dbb1c71e3
ThotDestroyerXXX <151725216+ThotDestroyerXXX@users.noreply.github.com>
2025-03-05 22:44:41 +0700

    change bisa regis organisasi (tapi gk fungsional akowkawakowk)

--- src/app/Pages/signUpOrg/page.tsx
+++ src/app/Pages/signUpOrg/page.tsx
@@ -3,13 +3,13 @@ import Image from "next/image";
 import { useSignUp } from "~/app/api/SignUp/SignUp";
 import chad from "~/app/Assets/chad.jpg";
 import SignForm from "~/app/Components/signForm";
+import TerritoryForm from "~/app/Components/territoryForm";
 
 const SignUpOrg = () => {
   const { handleSubmit, isPending, errors } = useSignUp();
-
   return (
     <div className="flex flex-col items-center justify-center">
-      <div className="flex w-full flex-col items-center gap-4 bg-cream pb-5 pt-5">
+      <div className="flex h-full w-full flex-col items-center gap-4 bg-cream pb-5 pt-5">
         <Image
           src={chad}
           alt="chad"
@@ -17,20 +17,48 @@ const SignUpOrg = () => {
         />
         <h2 className="text-3xl font-bold">Sign Up for Free</h2>
         <form
-          onSubmit={() => handleSubmit}
-          className="flex flex-col items-center gap-3 rounded-lg border-[1px] border-black bg-sageGreen p-10 shadow-lg"
+          onSubmit={(e: React.FormEvent<HTMLFormElement>) =>
+            handleSubmit(true, e)
+          }
+          className="flex flex-col items-center gap-6 rounded-lg border-[1px] border-black bg-sageGreen p-10 shadow-lg"
         >
           <SignForm
-            label={["Username", "Email", "Password"]}
-            type={["text", "text", "password"]}
+            label={[
+              "Username",
+              "Nama Organisasi",
+              "email",
+              "Nomor Telepon Organisasi",
+              "Password",
+              "Tanggal Berdiri Organisasi",
+            ]}
+            type={["text", "text", "text", "text", "password", "date"]}
             placeholder={[
-              "Enter your username",
-              "Enter your email",
-              "Enter your password",
+              "Masukkan username Anda",
+              "Masukkan Nama Organisasi Anda",
+              "Masukkan email Anda",
+              "Masukkan nomor telepon Organisasi Anda",
+              "Masukkan password Anda",
+              "Masukkan tanggal berdiri Organisasi Anda",
             ]}
             disabled={isPending}
             error={errors}
           />
+          <TerritoryForm />
+          <div className="flex w-screen max-w-sm flex-col">
+            <label
+              htmlFor="deskripsiOrganisasi"
+              className="text-sm text-gray-700"
+            >
+              Deskripsi Organisasi (100 - 500 kata)
+            </label>
+            <textarea
+              id="deskripsiOrganisasi"
+              name="deskripsiOrganisasi"
+              maxLength={500}
+              minLength={100}
+              disabled={isPending}
+            />
+          </div>
           <button
             type="submit"
             disabled={isPending}
--- src/app/api/SignUp/SignUp.ts
+++ src/app/api/SignUp/SignUp.ts
@@ -8,12 +8,39 @@ export const useSignUp = () => {
   const [errors, setErrors] = useState<string | null>(null);
   const [isPending, setIsPending] = useState<boolean>(false);
 
-  const handleSubmit = async (e: React.FormEvent<HTMLFormElement>) => {
+  const handleSubmit = async (
+    isOrganization: boolean,
+    e: React.FormEvent<HTMLFormElement>,
+  ) => {
     e.preventDefault();
     const form = new FormData(e.currentTarget);
     const username = form.get("username") as string;
     const email = form.get("email") as string;
     const password = form.get("password") as string;
+
+    if (isOrganization) {
+      const organizationName = form.get("namaorganisasi") as string;
+      const organizationPhone = form.get("nomorteleponorganisasi") as string;
+      const organizationEstablished = form.get(
+        "tanggalberdiriorganisasi",
+      ) as string;
+      const organizationDescription = form.get("deskripsiOrganisasi") as string;
+      const organizationProvince = form.get("province-select") as string;
+      const organizationCity = form.get("regency-select") as string;
+      const organizationDistrict = form.get("subdistrict-select") as string;
+      console.log(
+        organizationProvince,
+        organizationCity,
+        organizationDistrict,
+        organizationDescription,
+        organizationEstablished,
+        organizationName,
+        organizationPhone,
+        username,
+        email,
+        password,
+      );
+    }
 
     await authClient.signUp.email(
       {
@@ -26,6 +53,7 @@ export const useSignUp = () => {
           setIsPending(true);
         },
         onSuccess: () => {
+          // const userId =
           setIsPending(false);
           router.push("/");
           router.refresh();
