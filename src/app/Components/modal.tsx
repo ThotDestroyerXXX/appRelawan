8fed7ce576e63ac2c0d9f3b3616d2f402f83963b
ThotDestroyerXXX <151725216+ThotDestroyerXXX@users.noreply.github.com>
2025-03-07 13:34:55 +0700

    creating middleware and template dashboard (empty page)

--- src/app/Components/modal.tsx
+++ src/app/Components/modal.tsx
@@ -15,7 +15,7 @@ const Modal = ({ isOpen, onClose, children }: ModalProps) => {
       className="fixed inset-0 flex cursor-default items-center justify-center bg-black bg-opacity-50"
       onClick={onClose}
     >
-      <div className="relative flex w-full max-w-[30rem] flex-col gap-6 rounded-lg bg-white p-6 shadow-lg">
+      <div className="relative flex w-full max-w-[30rem] flex-col gap-6 rounded-lg bg-white p-6 text-black shadow-lg">
         <Link
           href="/Pages/signIn"
           className="absolute right-5 top-3 text-xl font-bold text-gray-500 hover:text-gray-700"
