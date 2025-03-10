036b8a3107a7edee7bb614f268cdaf361ce95d64
ThotDestroyerXXX <151725216+ThotDestroyerXXX@users.noreply.github.com>
2025-03-10 22:30:27 +0700

    fixed middleware (now can fetch from db)

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,8 +1,17 @@
 import { type NextRequest, NextResponse } from "next/server";
-import { getSessionCookie } from "better-auth";
+import { betterFetch } from "@better-fetch/fetch";
+import { type Session } from "./lib/auth";
 
 export async function middleware(request: NextRequest) {
-  const session = getSessionCookie(request);
+  const { data: session } = await betterFetch<Session>(
+    "/api/auth/get-session",
+    {
+      baseURL: request.nextUrl.origin,
+      headers: {
+        cookie: request.headers.get("cookie") ?? "", // Forward the cookies from the request
+      },
+    },
+  );
 
   if (session && request.nextUrl.pathname.startsWith("/Pages/Auth/")) {
     return NextResponse.redirect(new URL("/", request.url));
@@ -14,6 +23,11 @@ export async function middleware(request: NextRequest) {
     return NextResponse.redirect(new URL("/Pages/Auth/signIn", request.url));
   } else if (request.nextUrl.pathname === "/") {
     return NextResponse.redirect(new URL("/Pages/Home", request.url));
+  } else if (
+    !session?.user.organization_id &&
+    request.nextUrl.pathname.startsWith("/Pages/Organization/")
+  ) {
+    return NextResponse.redirect(new URL("/Pages/User/Dashboard", request.url));
   }
 }
 
